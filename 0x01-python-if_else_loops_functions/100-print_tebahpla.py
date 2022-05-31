0d4e999d0baf905c3e30060b0078ea06c44d0737
Qllela <sylvillela8@gmail.com>
2022-05-31 03:20:05 -0700

    python if, loops

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for l in range(ord("z"), ord("a") - 1, -1):
-print("{}".format(chr(l - (32 if l % 2 == 1 else 0))), end="")
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
